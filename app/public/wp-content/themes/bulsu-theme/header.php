0fefa6d71ec8cc9d118cb766326c831b28e615e0
Dandee Galang <dandeegalang.ph@gmail.com>
2025-06-09 15:54:23 +0800

    added landing page

---
+++ app/public/wp-content/themes/bulsu-theme/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <header class="site-header">
+      <div class="container">
+        <h1 class="school-logo-text float-left">
+          <a href="#"><strong>BULSU</strong> Community</a>
+        </h1>
+        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+        <div class="site-header__menu group">
+          <nav class="main-navigation">
+            <ul>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Programs</a></li>
+              <li><a href="#">Events</a></li>
+              <li><a href="#">Campuses</a></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
+          </nav>
+          <div class="site-header__util">
+            <a href="#" class="btn btn--small btn--red float-left push-right" style="color: gold; font-weight: bolder">Login</a>
+            <a href="#" class="btn btn--small btn--gold float-left" style="color: red; font-weight: bolder">Sign Up</a>
+            <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+          </div>
+        </div>
+      </div>
+    </header>
